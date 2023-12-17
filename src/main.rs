52077e5f55d394b554649df5366a7fcbbdf3c02d
Michael Quintero <michaelq.quintero@gmail.com>
2023-12-17 12:02:10 -0500

    fix dotenv approach but beginning to implement using secrets manager

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,67 @@
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
-// use std::env;
-// use dotenv::dotenv;
+use std::env;
+use dotenv::dotenv;
+/* get secret example in java\\\\]]]]]]]]]]]]\
+
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample
+// code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/home.html
+
+// Make sure to import the following packages in your code
+// import software.amazon.awssdk.regions.Region;
+// import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+// import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+// import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;	
+
+public static void getSecret() {
+
+    String secretName = "michaels-art-site-db";
+    Region region = Region.of("us-east-1");
+
+    // Create a Secrets Manager client
+    SecretsManagerClient client = SecretsManagerClient.builder()
+            .region(region)
+            .build();
+
+    GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+            .secretId(secretName)
+            .build();
+
+    GetSecretValueResponse getSecretValueResponse;
+
+    try {
+        getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+    } catch (Exception e) {
+        // For a list of exceptions thrown, see
+        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        throw e;
+    }
+
+    String secret = getSecretValueResponse.secretString();
+
+    // Your code goes here.
+} */
+
+
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // important todo: on a lambda this seems risky to do, but I'm not sure how to get the connection string otherwise.
-    // let POSTGRES_DB_HOST = env::var("POSTGRES_DB_HOST").expect("Error: Working directory environment variable POSTGRES_DB_HOST not found");
-    // let POSTGRES_DB_PORT = env::var("POSTGRES_DB_PORT").expect("Error: Working directory environment variable POSTGRES_DB_PORT not found");
-    // let POSTGRES_DB_NAME = env::var("POSTGRES_DB_NAME").expect("Error: Working directory environment variable POSTGRES_DB_NAME not found");
-    // let POSTGRES_DB_USER = env::var("POSTGRES_DB_USER").expect("Error: Working directory environment variable POSTGRES_DB_USER not found");
-    // let POSTGRES_DB_PASSWORD = env::var("POSTGRES_DB_PASSWORD").expect("Error: Working directory environment variable POSTGRES_DB_PASSWORD not found");
+    dotenv().ok();
     
-    // let connection_string = format!("postgres://{}:{}@{}:{}/{}",
-    // POSTGRES_DB_USER,
-    // POSTGRES_DB_PASSWORD,
-    // POSTGRES_DB_HOST,
-    // POSTGRES_DB_PORT,
-    // POSTGRES_DB_NAME);
+    let postgres_db_host = env::var("POSTGRES_DB_HOST").expect("Error: Working directory environment variable POSTGRES_DB_HOST not found");
+    let postgres_db_port = env::var("POSTGRES_DB_PORT").expect("Error: Working directory environment variable POSTGRES_DB_PORT not found");
+    let postgres_db_name = env::var("POSTGRES_DB_NAME").expect("Error: Working directory environment variable POSTGRES_DB_NAME not found");
+    let postgres_db_user = env::var("POSTGRES_DB_USER").expect("Error: Working directory environment variable POSTGRES_DB_USER not found");
+    let postgres_db_password = env::var("POSTGRES_DB_PASSWORD").expect("Error: Working directory environment variable POSTGRES_DB_PASSWORD not found");
+    
+    let connection_string = format!("postgres://{}:{}@{}:{}/{}",
+    postgres_db_user,
+    postgres_db_password,
+    postgres_db_host,
+    postgres_db_port,
+    postgres_db_name);
 
-    // println!("{}", connection_string);
     // Extract some useful information from the request
     let who = event
         .query_string_parameters_ref()
@@ -30,8 +74,8 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body(message.into())
-        // .body(connection_string.into())
+        // .body(message.into())
+        .body(connection_string.into())
         .map_err(Box::new)?;
     Ok(resp)
 }
